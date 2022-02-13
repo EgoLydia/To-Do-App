21178edbecf732c3f2e5627d079727761685ee9a
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-13 22:30:27 +0100

    Feature: Add create task function

--- script.js
+++ script.js
@@ -5,11 +5,27 @@
   const items = await getItems();
   console.log(items);
   for (let item in items) {
+    const li = createElementForTask(items[item]);
+    tasks.appendChild(li);
+  function createElementForTask(item) {
+    const template = document.getElementById("taskTemplate");
+    const newListItem = template.content.cloneNode(true);
+
+    const checkbox = newListItem.querySelector(".item-check");
+    const text = newListItem.querySelector(".item-text");
+    text.innerText = item.value;
+    checkbox.checked = item.complete;
+
+    };
+
+    return newListItem;
   function addNewItem() {
     const task = {
       value: taskinput.value,
       complete: false,
     };
+    let newItem = createElementForTask(task);
+    tasks.appendChild(newItem);
 
     taskinput.value = "";
     taskinput.focus();
