93f11e80cec47839fee24b3ecf4cbf2f6ab68007
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-13 22:26:18 +0100

    Feature: Add new item function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
   const taskinput = document.getElementById("new");
   const addbutton = document.getElementById("add");
   const tasks = document.getElementById("tasks");
+  addbutton.addEventListener("click", addNewItem);
+  const items = await getItems();
+  console.log(items);
+  for (let item in items) {
+  function addNewItem() {
+    const task = {
+      value: taskinput.value,
+      complete: false,
+    };
+
+    taskinput.value = "";
+    taskinput.focus();
